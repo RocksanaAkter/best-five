df3076669991fc9592b1e681707564e2972737a9
RocksanaAkter <rocksanaomi1176@gmail.com>
2022-08-28 09:10:38 +0600

    bonus

--- js/select.js
+++ js/select.js
@@ -4,27 +4,30 @@ function playerList(playerName) {
 
     const tableBody = document.getElementById('table-playerName')
     tableBody.innerHTML = "";
+
     for (let i = 0; i < playerName.length; i++) {
         const name = playerListArray[i];
-
-        if (i === 5) {
-            alert('you can not select more than 5 players')
-            break;
-        }
         const tr = document.createElement("tr")
         tr.innerHTML = `<th>${i + 1}</th>
 <th>${name}</th>`;
         tableBody.appendChild(tr);
     }
 
-
 }
 
 // step1: scan player name
 function selectPlayer(element) {
-    const playerName = element.parentNode.parentNode.children[0].innerText;
-    playerListArray.push(playerName);
-    playerList(playerListArray);
+    if (playerListArray.length < 5) {
+        const playerName = element.parentNode.parentNode.children[0].innerText;
+        playerListArray.push(playerName);
+        playerList(playerListArray);
+        // button disable 
+        element.disable = 'true';
+        element.style.background = 'gray';
+    }
+    else alert('you can not select more than 5 players')
+
+
 }
 
 //step3: calculate total player cost
