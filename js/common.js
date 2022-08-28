cd4de13cc1972c01c10ddfb6439bf6627f1c186d
RocksanaAkter <rocksanaomi1176@gmail.com>
2022-08-28 12:45:05 +0600

    last

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 function common(idValue) {
     const input = document.getElementById(idValue)
     const inputString = input.value;
-    const inputFloat = parseFloat(inputString)
+    const inputFloat = parseFloat(inputString) // convert input field value to float value
     return inputFloat;
 }
 
 function commonText(idValue) {
     const input = document.getElementById(idValue)
     const inputString = input.innerText;
-    const inputFloat = parseFloat(inputString);
+    const inputFloat = parseFloat(inputString);//convert innerText to float value
     return inputFloat;
 
 }
