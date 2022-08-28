9744c340562100b0184ed3c35ef931d926cd4736
RocksanaAkter <rocksanaomi1176@gmail.com>
2022-08-28 08:28:40 +0600

    bonus1

--- js/select.js
+++ js/select.js
@@ -1,33 +1,33 @@
 const playerListArray = [];
-// add player name to the list 
+//step2: add player name to the list 
 function playerList(playerName) {
 
     const tableBody = document.getElementById('table-playerName')
     tableBody.innerHTML = "";
     for (let i = 0; i < playerName.length; i++) {
         const name = playerListArray[i];
+
         if (i === 5) {
             alert('you can not select more than 5 players')
+            break;
         }
         const tr = document.createElement("tr")
         tr.innerHTML = `<th>${i + 1}</th>
 <th>${name}</th>`;
         tableBody.appendChild(tr);
     }
-    console.log('ok')
-
 
 
 }
 
-//  scan player name
+// step1: scan player name
 function selectPlayer(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
     playerListArray.push(playerName);
     playerList(playerListArray);
 }
 
-
+//step3: calculate total player cost
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const perPlayerCost = common('per-player-cost');
 
@@ -38,6 +38,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     totalPlayerCost.innerText = total;
 
 })
+//step3: calculate total cost
 document.getElementById('calculate-total-btn').addEventListener('click', function () {
     const managerCost = common('manager-cost');
     const coachCost = common('coach-cost');
